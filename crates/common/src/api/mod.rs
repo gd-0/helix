e720c56324061aa8d899d2fc6ab573a4304637d8
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2024-09-06 14:50:02 +0200

    chore(api): constraints api path consts

--- crates/common/src/api/mod.rs
+++ crates/common/src/api/mod.rs
@@ -16,10 +16,19 @@ pub(crate) const PATH_STATUS: &str = "/status";
 pub(crate) const PATH_REGISTER_VALIDATORS: &str = "/validators";
 pub(crate) const PATH_GET_HEADER: &str = "/header/:slot/:parent_hash/:pubkey";
 pub(crate) const PATH_GET_PAYLOAD: &str = "/blinded_blocks";
-
+pub(crate) const PATH_GET_HEADER_WITH_PROOFS: &str =
+    "/header_with_proofs/:slot/:parent_hash/:pubkey";
 
 pub(crate) const PATH_DATA_API: &str = "/relay/v1/data";
 
 pub(crate) const PATH_PROPOSER_PAYLOAD_DELIVERED: &str = "/bidtraces/proposer_payload_delivered";
 pub(crate) const PATH_BUILDER_BIDS_RECEIVED: &str = "/bidtraces/builder_blocks_received";
 pub(crate) const PATH_VALIDATOR_REGISTRATION: &str = "/validator_registration";
+
+pub(crate) const PATH_CONSTRAINTS_API: &str = "/constraints/v1";
+
+pub(crate) const PATH_SUBMIT_BUILDER_CONSTRAINTS: &str = "/builder/constraints";
+pub(crate) const PATH_DELEGATE_SUBMISSION_RIGHTS: &str = "/builder/delegate";
+pub(crate) const PATH_REVOKE_SUBMISSION_RIGHTS: &str = "/builder/revoke";
+pub(crate) const PATH_BUILDER_CONSTRAINTS: &str = "/relay/builder/constraints";
+pub(crate) const PATH_BUILDER_CONSTRAINTS_STREAM: &str = "/relay/builder/constraints_stream";
