273db553fdc366b15723822f0d37bfc0734cba15
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2024-10-04 12:28:39 +0200

    fix: sleep duration before validator refresh

--- crates/housekeeper/src/housekeeper.rs
+++ crates/housekeeper/src/housekeeper.rs
@@ -33,7 +33,8 @@ const TRUSTED_PROPOSERS_UPDATE_FREQ: u64 = 5;
 // Constants for known validators refresh logic.
 const MIN_SLOTS_BETWEEN_UPDATES: u64 = 6;
 const MAX_SLOTS_BEFORE_FORCED_UPDATE: u64 = 32;
-pub(crate) const SLEEP_DURATION_BEFORE_REFRESHING_VALIDATORS: Duration = Duration::from_secs(6);
+// NOTE: this should probably be set at config time
+pub(crate) const SLEEP_DURATION_BEFORE_REFRESHING_VALIDATORS: Duration = Duration::from_millis(500);
 
 // Max time between header and payload for OptimsiticV2 submissions
 const MAX_DELAY_BETWEEN_V2_SUBMISSIONS_MS: u64 = 2_000;
